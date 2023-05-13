2735ce6665eb957e1dc053319fa9db3c6a0fe324
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-05-13 11:35:57 +0900

    feat: useSearchParams로 query string 전달
    edit 페이지에서 name과 value로 엮인 데이터 전달
    useSearchParams 훅은 두 개의 인자를 가진 배열을 반환하는데,
    첫번째 인자는 전달 받은 query string 데이터를 get 함수로 받을 수 있는 용도,
    두번째 인자는 query string 데이터를 변환할 때 쓰는 용도의 함수

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,6 +1,19 @@
+import { useSearchParams } from 'react-router-dom';
+
 const Edit = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get('id');
+  const mode = searchParams.get('mode');
+
+  console.log('id: ', id);
+  console.log('mode: ', mode);
+
   return (
     <div>
+      <button onClick={() => setSearchParams({ who: 'SujinJo' })}>
+        Change QS
+      </button>
       <h1>Edit</h1>
       <p>이곳은 일기 수정 페이지 입니다.</p>
     </div>
