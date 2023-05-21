ccb43d79dcd7f3f90ba06ecc0dfd21b3e270a260
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-05-21 22:01:57 +0900

    feat: 새 일기 쓰기 버튼 구현
    useNavigate 훅 이용해 버튼 클릭시 /new 경로로 이동

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import MyButton from './MyButton';
 
 const optionList = [
   { name: '최신순', value: 'latest' },
@@ -36,6 +39,8 @@ const EmotionMenu = ({ value, onChange, filterOptionList }) => {
 };
 
 const DiaryList = ({ diaryList }) => {
+  const navigate = useNavigate();
+
   const [sortType, setSortType] = useState('latest');
   const [emoType, setEmoType] = useState('all');
 
@@ -77,6 +82,11 @@ const DiaryList = ({ diaryList }) => {
         value={emoType}
         onChange={setEmoType}
         filterOptionList={filterOptionList}
+      />
+      <MyButton
+        type={'positive'}
+        text={'새 일기 쓰기'}
+        onClick={() => navigate('/new')}
       />
       {getProccessedDiaryList().map((it) => (
         <div key={it.id}>
