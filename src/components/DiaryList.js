1f320a6b3f49c2c7f963519aa2564ee38d4aa477
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-05-21 21:16:07 +0900

    feat: 정렬 필터 구현
    select의 value가 '최신순'이면 내림차순, '오래된 순'이면 오름차순으로
    일기 데이터  정렬

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,7 +1,47 @@
+import { useState } from 'react';
+
+const optionList = [
+  { name: '최신순', value: 'latest' },
+  { name: '오래된 순', value: 'oldest' },
+];
+
+const ControlMenu = ({ value, onChange, optionList }) => {
+  return (
+    <select value={value} onChange={(e) => onChange(e.target.value)}>
+      {optionList.map((it, idx) => (
+        <option key={idx} value={it.value}>
+          {it.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
 const DiaryList = ({ diaryList }) => {
+  const [sortType, setSortType] = useState('latest');
+
+  const getProccessedDiaryList = () => {
+    const compare = (a, b) => {
+      if (sortType === 'latest') {
+        return parseInt(b.date) - parseInt(a.date);
+      } else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    };
+
+    const copyList = JSON.parse(JSON.stringify(diaryList));
+    const sortedList = copyList.sort(compare);
+    return sortedList;
+  };
+
   return (
     <div>
-      {diaryList.map((it) => (
+      <ControlMenu
+        value={sortType}
+        onChange={setSortType}
+        optionList={optionList}
+      />
+      {getProccessedDiaryList().map((it) => (
         <div key={it.id}>{it.content}</div>
       ))}
     </div>
