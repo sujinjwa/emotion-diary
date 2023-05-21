08fb21d1aae94c0ce7c9f416c2a92e2c3355e1d5
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-05-21 22:41:24 +0900

    feat: DiaryItem 내 날짜 및 일기 preview 구현

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -1,4 +1,9 @@
 const DiaryItem = ({ id, content, emotion, date }) => {
+  const env = process.env;
+  env.PUBLIC_URL = env.PUBLIC_URL || '';
+
+  const strDate = new Date(parseInt(date)).toLocaleDateString();
+
   return (
     <div className='DiaryItem'>
       <div
@@ -12,7 +17,10 @@ const DiaryItem = ({ id, content, emotion, date }) => {
           alt=''
         />
       </div>
-      <div></div>
+      <div className='info_wrapper'>
+        <div className='diary_date'>{strDate}</div>
+        <div className='diary_content_preview'>{content.slice(0, 25)}</div>
+      </div>
       <div></div>
     </div>
   );
