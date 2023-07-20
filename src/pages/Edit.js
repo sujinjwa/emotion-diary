f94b6d42de5c5a31e356d81a28d9fa4e69ea5177
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-07-20 11:42:56 +0900

    feat: 일기 상세 페이지 구현

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -19,6 +19,7 @@ const Edit = () => {
       if (targetDiary) {
         setOriginData(targetDiary);
       } else {
+        alert('없는 일기입니다.');
         navigate('/', { replace: true });
       }
     }
